9230eb9916799b7539b6d399daeeb974b0d3b6e6
Chadie Gil <caugis@gmail.com>
2024-12-20 13:39:05 +0800

    added upload for post

--- src/controller/post/post-controller.ts
+++ src/controller/post/post-controller.ts
@@ -1,3 +1,5 @@
+import fs from "fs"
+import path from "path"
 import { type Request, type Response } from "express"
 import * as PostService from "../../service/post/post-service"
 import CustomError from "../../utils/custom-error"
@@ -17,11 +19,17 @@ export const index = async (req: Request, res: Response) => {
 
 export const createPost = async (req: Request, res: Response) => {
   try {
-    const { description, attachment } = req.body
+    const { description } = req.body
     const userId = req.user.id
     if (!description) {
       res.status(400).json({ message: "Description is required." })
     }
+
+    let attachment = ""
+    if (req.file) {
+      attachment = req.file.filename
+    }
+
     const newPost = await PostService.create(userId, description, attachment)
     res.json(newPost)
   } catch (error) {
@@ -31,32 +39,79 @@ export const createPost = async (req: Request, res: Response) => {
 
 export const updatePost = async (req: Request, res: Response) => {
   try {
-    const { description, attachment } = req.body
+    const { description } = req.body
     const postId = req.params.id
-
     const userId = req.user.id
-    const updatePost = await PostService.updateById(
+
+    // Fetch the current post to get the attachment path
+    const currentPost = await PostService.getById(parseInt(postId))
+
+    if (!currentPost) {
+      throw new CustomError("Post not found", 404)
+    }
+
+    if (req.file && currentPost.attachment) {
+      const oldAttachmentPath = path.join(
+        __dirname,
+        "../../../uploads",
+        currentPost.attachment
+      )
+
+      if (fs.existsSync(oldAttachmentPath)) {
+        fs.unlinkSync(oldAttachmentPath)
+      } else {
+        throw new CustomError("Old attachment not found", 404)
+      }
+    }
+
+    const updatedPost = await PostService.updateById(
       userId,
       parseInt(postId),
       description,
-      attachment
+      req.file ? req.file.filename : currentPost.attachment // Update with the new file or keep the existing one
     )
-    res.json(updatePost)
+
+    res.json(updatedPost)
   } catch (error) {
-    res.status(500).json({ message: "Something went wrong." })
+    if (error instanceof CustomError) {
+      res.status(error.status).json({ message: error.message })
+    } else {
+      console.log(error)
+      res.status(500).json({ message: "Something went wrong." })
+    }
   }
 }
-
 export const deletePost = async (req: Request, res: Response) => {
   try {
     const userId = req.user.id
     const postId = parseInt(req.params.id)
+
+    const post = await PostService.getById(postId)
+    if (!post) {
+      throw new CustomError("Post not found", 404)
+    }
+
+    if (post.attachment) {
+      const attachmentPath = path.join(
+        __dirname,
+        "../../../uploads",
+        post.attachment
+      )
+
+      if (fs.existsSync(attachmentPath)) {
+        fs.unlinkSync(attachmentPath)
+      } else {
+        console.log(`Attachment not found: ${attachmentPath}`)
+      }
+    }
     await PostService.deleteById(userId, postId)
-    res.json({ message: `Post deleted successfully id ${postId}` })
+    res.json({ message: `Post deleted successfully, ID: ${postId}` })
   } catch (error) {
     if (error instanceof CustomError) {
       res.status(error.status).json({ message: error.message })
+    } else {
+      console.log(error)
+      res.status(500).json({ message: "Something went wrong." })
     }
-    res.status(500).json({ message: "Something went wrong." })
   }
 }
--- src/routes/post/post-route.ts
+++ src/routes/post/post-route.ts
@@ -1,12 +1,18 @@
 import express from "express"
 import * as PostController from "../../controller/post/post-controller"
 
+import upload from "../../utils/multer-config"
+
 const router = express.Router()
 
 router.get("/", PostController.index)
 router.get("/post", PostController.getPost)
-router.post("/create", PostController.createPost)
-router.put("/update/:id", PostController.updatePost)
+router.post("/create", upload.single("attachment"), PostController.createPost)
+router.put(
+  "/update/:id",
+  upload.single("attachment"),
+  PostController.updatePost
+)
 router.delete("/delete/:id", PostController.deletePost)
 
 export default router
